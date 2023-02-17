0b13f5e1052c9b280e12bd9126508edb4bd8ab53
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-17 08:33:08 +0600

    new day

--- js/main.js
+++ js/main.js
@@ -4,32 +4,42 @@
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
 
 
-form.addEventListener('submit', function (event) {
-    /* отмена перезагрузки  */
-    event.preventDefault();
-    
-    /* Достаем текст задачи из поля инпут */
-    const taskText = taskInput.value;
+form.addEventListener('submit', addTask);
 
-    /* Формируем разметку для новой задачи */
+    function addTask(event) {
+        /* отмена перезагрузки  */
+        event.preventDefault();
+        
+        /* Достаем текст задачи из поля инпут */
+        const taskText = taskInput.value;
 
-    const taskHtml = `
-                <li class="list-group-item d-flex justify-content-between task-item">
-                    <span class="task-title">${taskText}</span>
-                    <div class="task-item__buttons">
-                        <button type="button" data-action="done" class="btn-action">
-                            <img src="./img/tick.svg" alt="Done" width="18" height="18">
-                        </button>
-                        <button type="button" data-action="delete" class="btn-action">
-                            <img src="./img/cross.svg" alt="Done" width="18" height="18">
-                        </button>
-                    </div>
-                </li>`;
-                
-    /* Добавляем задачу на страницу */
-    tasksList.insertAdjacentHTML('beforeend', taskHtml);
+        /* Формируем разметку для новой задачи */
+
+        const taskHtml = `
+                    <li class="list-group-item d-flex justify-content-between task-item">
+                        <span class="task-title">${taskText}</span>
+                        <div class="task-item__buttons">
+                            <button type="button" data-action="done" class="btn-action">
+                                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                            </button>
+                            <button type="button" data-action="delete" class="btn-action">
+                                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                            </button>
+                        </div>
+                    </li>`;
+                    
+        /* Добавляем задачу на страницу */
+        tasksList.insertAdjacentHTML('beforeend', taskHtml);
+
+        /* Очищаем поле ввода и возвращаем на него фокус */
+        taskInput.value = '';
+        taskInput.focus();
 
 
-});
+        if (emptyList.children.length > 1) {
+            emptyList.classList.add('none');
+        }   
+    }
